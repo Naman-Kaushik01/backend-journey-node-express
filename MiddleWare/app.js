77e8c0c1c1d7d4e65f64acd56853c4a0241ec04d
Naman Kaushik <kaushiknaman68@gmail.com>
2025-11-14 22:43:50 +0530

    middleware

--- MiddleWare/app.js
+++ MiddleWare/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./ExpressError");
 
 //middleware -> response send
 // app.use((req,res,next)=>{
@@ -9,29 +10,40 @@ const app = express();
 // })
 
 //Logger -morgan
-app.use((req,res,next)=>{
-    req.time =new Date(Date.now()).toString();
-    console.log(req.method , req.hostname , req.time);
-    next();
-})
-app.get("/" ,(req , res)=>{
-    res.send("Hii I am root");
-})
-app.get("/random" ,(req,res)=>{
-    res.send("this is a random page");
-});
+// app.use((req,res,next)=>{
+//     req.time =new Date(Date.now()).toString();
+//     console.log(req.method , req.hostname , req.time);
+//     next();
+// })
+// app.get("/" ,(req , res)=>{
+//     res.send("Hii I am root");
+// })
+// app.get("/random" ,(req,res)=>{
+//     res.send("this is a random page");
+// });
 
-app.get("/api" ,(req,res)=>{
-    res.send("data");
-})
+// app.get("/api" ,(req,res)=>{
+//     res.send("data");
+// })
 //Error -404
 app.use("/api" ,(req,res,next)=>{
     let {token} =req.query;
     if(token =="giveaccess"){
         next();
     }
-    res.send("ACCESS DENIED");
+    throw new ExpressError(401,"Access Denied");
 })
+
+app.get("/err",(req,res,next)=>{
+    abcd=abcd;
+});
+app.get("/admin", (req , res)=>{
+    throw new ExpressError(403 ,"Access for admin is denied");
+})
+app.use((err,req,res,next)=>{
+    let {status =500 , message ="Some Error Occured"} = err;
+    res.status(status).send(message);   
+});
 app.use((req,res)=>{
    res.status(404).send("Page not found");
 })
